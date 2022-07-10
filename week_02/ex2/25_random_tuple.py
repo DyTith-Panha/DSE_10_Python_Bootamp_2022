5d6de6f380861f9f2d0e002d2c49e76ebcb2b8ad
DyTith-Panha <dytithpanha21@kit.edu.kh>
2022-07-11 02:18:04 +0700

    I've done

--- week_02/ex2/25_random_tuple.py
+++ week_02/ex2/25_random_tuple.py
@@ -3,7 +3,7 @@ def random_tuple(numbers):
     random_numbers = random.randint(0,100)
     idk = []
     sum = 0
-    for i in range(0, numbers):
+    for i in range(0, numbers   ):
         random_numbers = random.randint(0, 100)
         print(f"random number {i} : {random_numbers}")
         idk.append(random_numbers)
