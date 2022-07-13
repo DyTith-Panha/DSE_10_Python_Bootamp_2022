6ca47cea4f7b615f78db0eca321ac7c34f9a25dc
DyTith-Panha <dytithpanha21@kit.edu.kh>
2022-07-13 11:41:04 +0700

    Ive done

--- week_01/projects/01_dice.py
+++ week_01/projects/01_dice.py
@@ -1,47 +1,47 @@
 import random
 
 print("$ python 01_dice.py")
-rollNumber = input("Welcome to this dices game!\nEnter the number of dices you want to roll:")
+r_num = input("Welcome to this dices game!\nEnter the number of dices you want to roll:")
 result = 0
 
-if rollNumber.isnumeric():
+if r_num.isnumeric():
     while True:
-        if int(rollNumber) == 1:
-            randomNumber = random.randint(1, 6)
-            result += randomNumber
+        if int(r_num) == 1:
+            ran_num = random.randint(1, 6)
+            result += ran_num
             print(f"RESULT : {result}")
             break
-        elif (int(rollNumber) >= 2) and (int(rollNumber) <= 8):
-            for i in range(1, int(rollNumber) + 1):
-                randomNumber = random.randint(1, 6)
-                print(f"Dice {i} : {randomNumber}")
-                result += randomNumber
+        elif (int(r_num) >= 2) and (int(r_num) <= 8):
+            for i in range(1, int(r_num) + 1):
+                ran_num = random.randint(1, 6)
+                print(f"Dice {i} : {ran_num}")
+                result += ran_num
             print(f"==========")
             print(f"RESULT : {result}")
             print(f"=" * 10)
             break
-        elif (int(rollNumber) <= 0) or (int(rollNumber) >= 9):
+        elif (int(r_num) <= 0) or (int(r_num) >= 9):
             print("USAGE: The number must be between 1 and 8")
-            rollNumber = input("Enter the number of dices you want to roll")
+            r_num = input("Enter the number of dices you want to roll")
 else:
     while True:
         print("USAGE: The number must be between 1 and 8")
-        rollNumberAgain = input("Enter the number of dices you want to roll")
-        if rollNumberAgain.isnumeric():
-            if int(rollNumberAgain) == 1:
-                randomNumber = random.randint(1, 6)
-                result += randomNumber
+        r_num_again = input("Enter the number of dices you want to roll")
+        if r_num_again.isnumeric():
+            if int(r_num_again) == 1:
+                ran_num = random.randint(1, 6)
+                result += ran_num
                 print(f"RESULT : {result}")
                 break
-            elif (int(rollNumberAgain) >= 2) and (int(rollNumberAgain) <= 8):
-                for i in range(1, int(rollNumberAgain) + 1):
-                    randomNumber = random.randint(1, 6)
-                    print(f"Dice {i} : {randomNumber}")
-                    result += randomNumber
+            elif (int(r_num_again) >= 2) and (int(r_num_again) <= 8):
+                for i in range(1, int(r_num_again) + 1):
+                    ran_num = random.randint(1, 6)
+                    print(f"Dice {i} : {ran_num}")
+                    result += ran_num
                 print(f"==========")
                 print(f"RESULT : {result}")
                 print(f"=" * 10)
                 break
-            elif (int(rollNumberAgain) <= 0) or (int(rollNumberAgain) >= 9):
+            elif (int(r_num_again) <= 0) or (int(r_num_again) >= 9):
                 print("USAGE: The number must be between 1 and 8")
-                rollNumber = input("Enter the number of dices you want to roll")
+                r_num = input("Enter the number of dices you want to roll")
